ae851187c1974f2b6fd326c9ce74f744d6823203
sun.kening <sun.kening@ustcinfo.com>
2019-09-02 09:52:38 +0800

    观察者模式

---
+++ src/main/java/com/skn/Learning/designpatterndemo/observer/Observer.java
@@ -0,0 +1,12 @@
+package com.skn.Learning.designpatterndemo.observer;
+
+/**
+ * 观察者模式（以微信公众服务为例）
+ * 定义了对象之间的一对多的依赖，这样一来，当一个对象改变时，它的所有的依赖者都会收到通知并自动更新
+ * @author skn
+ * https://blog.csdn.net/lmj623565791/article/details/24179699
+ *
+ */
+public class Observer {
+
+}
